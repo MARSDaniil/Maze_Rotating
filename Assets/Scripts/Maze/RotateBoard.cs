f85b0a78259c8ed6ca70afbd7f0027021c512bd4
MARS <61511540+MARSMPEI@users.noreply.github.com>
2023-03-23 23:00:15 +0300

    mobile input
    add mobile  input

--- Assets/Scripts/Maze/RotateBoard.cs
+++ Assets/Scripts/Maze/RotateBoard.cs
@@ -8,14 +8,24 @@ public class RotateBoard : MonoBehaviour
 
     [SerializeField]  float speedOfRotation = 15;
 
-
+    private float screenHeight;
+    private float screenWidth;
     // Update is called once per frame
-    
+
 
     private void Start()
     {
+#if UNITY_STANDALONE || UNITY_WEBGL
+
+        if (Application.isMobilePlatform == true)
+        {
+            screenWidth = Screen.width;
+        }
+#endif
 #if UNITY_IOS || UNITY_ANDROID
-        Input.gyro.enabled = true;
+        screenWidth = Screen.width;
+        Debug.Log("Screen Height : " + Screen.height);
+        Debug.Log("Screen Width : " + Screen.width);
 #endif
 
     }
@@ -26,26 +36,46 @@ public class RotateBoard : MonoBehaviour
     {
 
 #if UNITY_STANDALONE || UNITY_WEBGL
-        if (Input.GetAxis("Horizontal") != 0)
+
+        if (Application.isMobilePlatform == true)
+        {
+            isTouch();
+        }
+        else if (Input.GetAxis("Horizontal") != 0)
         {
             Rotate(Input.GetAxis("Horizontal"));
         }
 #endif
-       
+
 #if UNITY_IOS || UNITY_ANDROID
-        /*
-         if (Input.gyro.attitude.eulerAngles.x != 0)
-         {
-             Debug.Log(Input.gyro.attitude);
-             Rotate(Input.gyro.attitude.eulerAngles.z);
-         }
-        */
-        Debug.Log(Input.gyro.attitude);
+   
+        isTouch();
+
+          
 #endif
     }
 
     void Rotate(float ditectionOfRotate)//rotate plate
     {
         Field.transform.Rotate(0, 0, -ditectionOfRotate * speedOfRotation * Time.fixedDeltaTime);
+    }
+
+    private void isTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 position = touch.position;
+            if (position.x > screenWidth / 2)
+            {
+                Rotate(-1);
+            }
+            if (position.x < screenWidth / 2)
+            {
+                Rotate(1);
+
+            }
+            Debug.Log("touch position - " + position);
+        }
     }
 }
