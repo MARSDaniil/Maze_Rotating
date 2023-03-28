6e558525999063e33b93ef0eb5b84e0321b4ba75
MARS <61511540+MARSMPEI@users.noreply.github.com>
2023-03-28 23:29:19 +0300

    localization, music botton, language botton switch
    start localization in info, add switcher music bottom, add language botton switcher

---
+++ Assets/Scripts/Localization/LocaleSelector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+public class LocaleSelector : MonoBehaviour
+{
+    private bool active = false;
+    public void ChangeLocale(int localID)
+    {
+        if (active == true)
+        {
+            return;
+        }
+        StartCoroutine(SetLocale(localID));
+    }
+    IEnumerator SetLocale(int _localId)
+    {
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localId];
+        active = false;
+    }
+}
