69909fca0f6f9677f05a8e9a1a50e673c63906a7
MARS <61511540+MARSMPEI@users.noreply.github.com>
2023-04-04 22:20:33 +0300

    delete hand-SDK, add YaSDK, add server save

---
+++ Assets/Scripts/LevelMenu/PlayerData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YG {
+
+    public class PlayerData : MonoBehaviour
+    {
+
+
+
+        public void HaHa()
+        {
+            int i = YandexGame.savesData.maxLevelOpen;
+        }
+
+    }
+}
---
+++ Assets/YandexGame/WorkingData/SavesYG.cs
@@ -0,0 +1,39 @@
+﻿
+namespace YG
+{
+    [System.Serializable]
+    public class SavesYG
+    {
+        // "Технические сохранения" для работы плагина (Не удалять)
+        public int idSave;
+        public bool isFirstSession = true;
+        public string language = "ru";
+        public bool promptDone;
+
+        // Тестовые сохранения для демо сцены
+        // Можно удалить этот код, но тогда удалите и демо (папка Example)
+        public int money = 1;                       // Можно задать полям значения по умолчанию
+        public string newPlayerName = "Hello!";
+        public bool[] openLevels = new bool[3];
+
+        // Ваши сохранения
+
+        public int maxLevelOpen = 0;
+        public bool isMusicOn = true;
+
+        public int selectedLanguage;
+        
+        // Поля (сохранения) можно удалять и создавать новые. При обновлении игры сохранения ломаться не должны
+        // Пока выявленное ограничение - это расширение массива
+
+
+        // Вы можете выполнить какие то действия при загрузке сохранений
+        public SavesYG()
+        {
+
+            maxLevelOpen = 0;
+            isMusicOn = true;
+
+        }
+    }
+}
