a55d75639ca30300f052864af4a4093a0963dd35
MARS <61511540+MARSMPEI@users.noreply.github.com>
2023-05-08 21:20:46 +0300

    info, anim botton
    change info(now info for mobile setActive for mobile, info for PC set Active for PC)
    
    delete animator of botton(YG bugs)

--- Assets/Scripts/UIScripts/InfoSwitcher.cs
+++ Assets/Scripts/UIScripts/InfoSwitcher.cs
@@ -5,7 +5,30 @@ using UnityEngine;
 public class InfoSwitcher : Switcher
 {
     public float timeOfWait;
-   public void Start()
+
+    public GameObject MobileText;
+    public GameObject PcText;
+
+    private void Awake()
+    {
+
+#if UNITY_STANDALONE || UNITY_WEBGL
+
+        if(Application.isMobilePlatform == true)
+        {
+            MobileText.SetActive(true);
+        }
+        else
+        {
+            PcText.SetActive(true);
+        }
+#endif
+#if UNITY_IOS || UNITY_ANDROID
+         MobileText.SetActive(true);
+#endif
+    }
+
+    private void Start()
     {
         StartCoroutine(WaitTime());
     }
